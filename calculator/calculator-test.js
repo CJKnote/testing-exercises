b7c1935e00507523fd3b704bfff1febd8a559979
Caroline Knote <cj.knote@gmail.com>
2021-12-16 19:01:20 -0800

    adding functionality and tests

--- calculator/calculator-test.js
+++ calculator/calculator-test.js
@@ -1,11 +1,23 @@
 
 it('should calculate the monthly rate correctly', function () {
-  // ...
+  const values = {amount: 5000, years: 10, rate: 5.5 };
+  expect(calculateMonthlyPayment(values)).toEqual('54.26');
+
 });
 
 
 it("should return a result with 2 decimal places", function() {
-  // ..
+  const values = {amount: 1075, years: 5, rate: 2.3};
+  expect(calculateMonthlyPayment(values)).toEqual('18.98');
 });
 
-/// etc
+it("should handle small loan amounts", function(){
+  const values = {amount: 5, years: 10, rate: 5.5};
+  expect(calculateMonthlyPayment(values)).toEqual('0.05');
+});
+
+it("should handle high interest rates", function(){
+  const values = {amount: 1000, years: 10, rate: 95};
+  expect(calculateMonthlyPayment(values)).toEqual('79.18');
+})
+
--- calculator/calculator.js
+++ calculator/calculator.js
@@ -21,20 +21,50 @@ function getCurrentUIValues() {
 // Put some default values in the inputs
 // Call a function to calculate the current monthly payment
 function setupIntialValues() {
+  const values = {amount: 5000, years: 10, rate: 5.5};
+  const amountInput = document.getElementById("loan-amount");
+  const yearInput = document.getElementById("loan-years");
+  const rateInput = document.getElementById("loan-rate");
+  amountInput.value = values.amount;
+  yearInput.value = values.years;
+  rateInput.value = values.rate;
+
+  update();
+
 }
 
 // Get the current values from the UI
 // Update the monthly payment
 function update() {
+  const amountInput = document.getElementById("loan-amount");
+  const yearInput = document.getElementById("loan-years");
+  const rateInput = document.getElementById("loan-rate");
+
+  const values = {amount: amountInput.value, years: yearInput.value, rate: rateInput.value};
+
+  const payment = calculateMonthlyPayment(values);
+
+  updateMonthly(payment);
+  
+
 }
 
 // Given an object of values (a value has amount, years and rate ),
 // calculate the monthly payment.  The output should be a string
 // that always has 2 decimal places.
 function calculateMonthlyPayment(values) {
+  const p = values.amount;
+  const i = values.rate/12/100;
+  const n = values.years * 12;
+  
+
+  return ((p*i)/(1-(Math.pow((1+i),-n)))).toFixed(2);
 }
+
 
 // Given a string representing the monthly payment value,
 // update the UI to show the value.
 function updateMonthly(monthly) {
+  const paymentDisplay = document.getElementById("monthly-payment");
+  paymentDisplay.innerText = "$" + monthly;
 }
